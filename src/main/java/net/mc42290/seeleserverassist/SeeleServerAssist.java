45956ee8eb52825d2ea1f08c8b5313a8cdffede0
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-02-19 16:41:57 +0900

    JOB_SYSTEM変数の実装(及びそれの取得関数)

--- src/main/java/net/mc42290/seeleserverassist/SeeleServerAssist.java
+++ src/main/java/net/mc42290/seeleserverassist/SeeleServerAssist.java
@@ -1,6 +1,7 @@
 package net.mc42290.seeleserverassist;
 
 import net.mc42290.seeleserverassist.damageEdit.DamageCalc;
+import net.mc42290.seeleserverassist.job.JobMainSystem;
 import net.mc42290.seeleserverassist.spItem.Attack;
 import net.mc42290.seeleserverassist.spItem.ReciveDamage;
 import net.mc42290.seeleserverassist.spItem.RightClick;
@@ -11,6 +12,7 @@ import java.util.logging.Logger;
 public final class SeeleServerAssist extends JavaPlugin {
     private static JavaPlugin plugin = null;
     private static Logger log = null;
+    private static JobMainSystem JOB_SYSTEM = null;
 
     @Override
     public void onEnable() {
@@ -19,12 +21,14 @@ public final class SeeleServerAssist extends JavaPlugin {
         log = getLogger();
 
         new DamageCalc(this);
+        JOB_SYSTEM = new JobMainSystem(this);
         new Attack(this);new ReciveDamage(this);new RightClick(this);
+        new Command(this);
     }
 
     public static JavaPlugin getInstance(){return plugin;}
-
     public static Logger getLog(){return log;}
+    public static JobMainSystem getJobSystem(){return JOB_SYSTEM;}
 
     @Override
     public void onDisable() {
