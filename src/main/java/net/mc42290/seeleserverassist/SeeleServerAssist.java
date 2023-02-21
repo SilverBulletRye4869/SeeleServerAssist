2ce35414697dbcdf37710d2adc8700678adeaf73
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-02-21 19:50:24 +0900

    フォルダセットアップ追加

--- src/main/java/net/mc42290/seeleserverassist/SeeleServerAssist.java
+++ src/main/java/net/mc42290/seeleserverassist/SeeleServerAssist.java
@@ -7,6 +7,11 @@ import net.mc42290.seeleserverassist.spItem.ReciveDamage;
 import net.mc42290.seeleserverassist.spItem.RightClick;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 public final class SeeleServerAssist extends JavaPlugin {
@@ -16,6 +21,14 @@ public final class SeeleServerAssist extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        if(!folderSetup()){
+            Util.sendConsole("プラグインフォルダの作成に失敗しました", Util.MessageType.ERROR);
+            this.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+
         // Plugin startup logic
         plugin = this;
         log = getLogger();
@@ -35,6 +48,48 @@ public final class SeeleServerAssist extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    private static void folderSetup(){
+    private boolean folderSetup(){
+        try{
+            Files.createDirectories(Paths.get(this.getDataFolder()+"/data"));
+            Files.createDirectories(Paths.get(this.getDataFolder()+"/log"));
+        }catch (IOException e){
+            Util.sendConsole("dataフォルダ又はlogフォルダの作成に失敗しました", Util.MessageType.ERROR);
+            e.printStackTrace();
+            return false;
+        }
+        File file = new File(this.getDataFolder()+"/README.txt");
+
+        try {
+            file.createNewFile();
+        }catch (IOException e){
+            if(Files.exists(Paths.get(this.getDataFolder()+"README.txt")))return true;
+            else{
+                Util.sendConsole("README.txtファイルの作成に失敗しました", Util.MessageType.ERROR);
+                e.printStackTrace();
+                return false;
+            }
+        }
+        if(!file.isFile()){
+            Util.sendConsole("README.txtはファイルではありません",Util.MessageType.ERROR);
+            return false;
+        }
+        if(!file.canWrite()){
+            Util.sendConsole("README.txtは書き込み不可のファイルです",Util.MessageType.ERROR);
+            return false;
+        }
+
+        try {
+            FileWriter fw = new FileWriter(file);
+            fw.write(
+                    "[注意] config.yml以外のファイルを手動で編集した場合の動作は保証しません\n" +
+                            "[Note] We do not guarantee the operation if files other than \"config.yml\" are edited manually."
+            );
+            fw.close();
+        }catch (IOException e){
+            Util.sendConsole("README.txtファイルの書き込みに失敗しました",Util.MessageType.ERROR);
+            return false;
+        }
+
+        return true;
     }
 }
