9ca86fbd5a2ef40f26a434af05de6f5d9f23dd72
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-02-22 12:35:09 +0900

    はじめてのこみっと

---
+++ src/main/java/net/mc42290/seeleserverassist/AssistCommand.java
@@ -0,0 +1,94 @@
+package net.mc42290.seeleserverassist;
+
+import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.NBTType;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class AssistCommand implements CommandExecutor {
+    public AssistCommand(JavaPlugin plugin){
+        PluginCommand command = plugin.getCommand("mc42290");
+        command.setExecutor(this);
+        command.setTabCompleter(new Tab());
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player))return true;
+        Player p = (Player) sender;
+        if(args.length==0){
+            //ヘルプ
+            return true;
+        }
+
+        ItemStack item = p.getInventory().getItemInMainHand();
+        switch (args[0]){
+            case "getnbt":
+                if(!sender.hasPermission("mc42290.admin"))return true;
+                if(item.getAmount() == 0)return true;
+
+                if(args.length > 1){
+                    NBTItem nbtItem = new NBTItem(item);
+                    if(!nbtItem.hasKey(args[1]))Util.sendPrefixMessage(p,"null");
+                    Util.sendPrefixMessage(p,"§d§l{ "+args[1]+" : "+nbtItem.getInteger(args[1])+" }");
+                }else Util.sendPrefixMessage(p," §d§l"+new NBTItem(item).toString());
+                break;
+            case "setnbt":
+                if(!sender.hasPermission("mc42290.admin"))return true;
+                if(item.getAmount() == 0 || args.length < 3 || !args[2].matches("\\d+"))return true;
+                p.getInventory().setItemInMainHand(new NBTItem(item){{
+                    setInteger(args[1],Integer.parseInt(args[2]));
+                }}.getItem());
+                Util.sendPrefixMessage(p,"§a§l手に持ってるアイテムにnbt§d§l{ "+args[1]+" : "+args[2]+" }§a§lを適用しました");
+                break;
+
+            case "getmodel":
+                ItemMeta meta = item.getItemMeta();
+                if(!sender.hasPermission("mc42290.admin") || meta==null)return true;
+                Util.sendPrefixMessage(p,"§a§l現在持っているアイテムのカスモデ番号: §d§l"+ (meta.hasCustomModelData() ? meta.getCustomModelData(): "null"));
+                break;
+
+            case "setmodel":
+                if(!sender.hasPermission("mc42290.admin") || item.getItemMeta()==null|| args.length < 2 || !args[1].matches("\\d+"))return true;
+                item.getItemMeta().setCustomModelData(Integer.parseInt(args[1]));
+                Util.sendPrefixMessage(p,"§a§l現在持っているアイテムのカスモデ番号を§d§l"+args[1]+"§a§lに変更しました");
+
+        }
+        return true;
+    }
+
+    private class Tab implements TabCompleter{
+
+        @Override
+        public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+            if(!(sender instanceof Player))return null;
+            Player p = (Player) sender;
+            switch (args.length){
+                case 1:
+                    if(sender.hasPermission("mc42290.admin"))return List.of("getnbt","setnbt","getmodel","setmodel");
+                    break;
+                case 2:
+                    switch (args[0]){
+                        case "getnbt":
+                            if(sender.hasPermission("mc42290.admin")){
+                                ItemStack item = p.getInventory().getItemInMainHand();
+                                if(item.getAmount() ==0)return null;
+                                NBTItem nbtItem = new NBTItem(item);
+                                return nbtItem.getKeys().stream()
+                                        .filter(g -> nbtItem.getType(g).equals(NBTType.NBTTagInt))
+                                        .collect(Collectors.toList())
+;                            }
+                    }
+
+            }
+            return null;
+        }
+    }
+}
