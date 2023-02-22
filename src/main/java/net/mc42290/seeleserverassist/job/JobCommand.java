1fd1e88fce8801fec6fc064ac3a7aa249059a442
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-02-22 12:33:53 +0900

    リファクタリング

--- src/main/java/net/mc42290/seeleserverassist/job/JobCommand.java
+++ src/main/java/net/mc42290/seeleserverassist/job/JobCommand.java
@@ -1,7 +1,8 @@
-package net.mc42290.seeleserverassist;
+package net.mc42290.seeleserverassist.job;
 
 import de.tr7zw.changeme.nbtapi.NBTItem;
-import net.mc42290.seeleserverassist.job.JobMainSystem;
+import net.mc42290.seeleserverassist.SeeleServerAssist;
+import net.mc42290.seeleserverassist.Util;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
@@ -18,9 +19,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Command implements CommandExecutor {
-    public Command(JavaPlugin plugin){
-        PluginCommand command = plugin.getCommand("mc42290");
+public class JobCommand implements CommandExecutor {
+    public JobCommand(JavaPlugin plugin){
+        PluginCommand command = plugin.getCommand("mc42290.job");
         if(command==null){
             Util.sendConsole("commandの登録に失敗しました", Util.MessageType.ERROR);
             return;
@@ -42,17 +43,18 @@ public class Command implements CommandExecutor {
 
         switch (args[0]){
             case "getticket":
-                if(!sender.hasPermission("mc42290.admin"))return true;
+                if(!sender.hasPermission("mc42290.admin.job"))return true;
+                int availableNum = args.length >= 2 && args[1].matches("\\d+") ? Integer.parseInt(args[1]) : 1;
                 ItemStack item = new NBTItem(new ItemStack(Material.PAPER)){{
-                    setInteger("jobchange", args.length >= 2 && args[1].matches("\\d+") ? Integer.parseInt(args[1]) : 1);
+                    setInteger("jobchange", availableNum);
                 }}.getItem();
 
                 p.getInventory().addItem(item);
-                Util.sendPrefixMessage(p,"§d§l職業選択券("+item.getAmount()+"回分)§a§lを取得しました");
+                Util.sendPrefixMessage(p,"§d§l職業選択券("+availableNum+"回分)§a§lを取得しました");
                 break;
 
             case "setjob":
-                if(!sender.hasPermission("mc42290.admin"))return true;
+                if(!sender.hasPermission("mc42290.admin.job"))return true;
                 if(args.length<3){
                     Util.sendPrefixMessage(p,"§c対象と、jobを正しく入力してください。");
                     return true;
@@ -75,7 +77,7 @@ public class Command implements CommandExecutor {
                 break;
 
             case "getjob":
-                if(!sender.hasPermission("mc42290.admin"))return true;
+                if(!sender.hasPermission("mc42290.admin.job"))return true;
                 if(args.length<2 || (target = Bukkit.getOfflinePlayer(args[1])) == null)return true;
             case "check":
                 if(target == null)target=p;
@@ -93,12 +95,13 @@ public class Command implements CommandExecutor {
         public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
             switch (args.length){
                 case 1:
-                    if(sender.hasPermission("mc42290.admin"))return List.of("getticket","setjob","getjob","check");
+                    if(sender.hasPermission("mc42290.admin.job"))return List.of("getticket","setjob","getjob","check");
                     else return List.of("check");
                 case 2:
                     switch (args[0]){
                         case "setjob":
                         case "getjob":
+                            if(sender.hasPermission("mc42290.admin.job"))
                             return Arrays.asList(Bukkit.getOfflinePlayers()).stream()
                                     .filter(g->g.getName().matches("^"+args[1]+".*$"))
                                     .map(e->e.getName())
@@ -108,6 +111,7 @@ public class Command implements CommandExecutor {
                 case 3:
                     switch (args[0]){
                         case "setjob":
+                            if(sender.hasPermission("mc42290.admin.job"))
                             return Arrays.asList(JobMainSystem.JOB.values()).stream()
                                     .filter(g -> g.name().matches("^" + args[2] + ".*$"))
                                     .map(e -> e.name())
