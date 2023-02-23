147ed35badd5d8b2579bf0e14f5ee65c8a372c21
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-02-23 14:03:27 +0900

    角度θを自由に指定できるように調整

--- src/main/java/net/mc42290/seeleserverassist/spItem/LeftClick.java
+++ src/main/java/net/mc42290/seeleserverassist/spItem/LeftClick.java
@@ -12,14 +12,10 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-
-import static net.mc42290.seeleserverassist.Util.getNBT;
+import static net.mc42290.seeleserverassist.Util.*;
 
 public class LeftClick implements Listener {
     private static final Set<String> cooltime = new HashSet<>();
@@ -29,7 +25,7 @@ public class LeftClick implements Listener {
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this,plugin);
     }
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onLeftClick(PlayerInteractEvent e){
         Player p = e.getPlayer();
         if(!(e.getAction().equals(Action.LEFT_CLICK_AIR) || e.getAction().equals(Action.LEFT_CLICK_BLOCK)))return;
@@ -37,20 +33,20 @@ public class LeftClick implements Listener {
         cooltime.add("interact_"+p);
         Bukkit.getScheduler().runTaskLater(plugin,()->cooltime.remove("interact_"+p),1);  //2回は知らないようにするやつ
 
-        Location loc = p.getLocation();
         int nbtM = Util.getNBT("adwe",e.getPlayer(), EquipmentSlot.HAND);
 
         switch (nbtM){
             case 12:
-                double reach = getNBT("reach",p,EquipmentSlot.HAND);
+                double reach = getNBT_lf("reach",p,EquipmentSlot.HAND,6);
+                double theta = getNBT_lf("theta",p,EquipmentSlot.HAND, Math.PI / 90);
                 double damage = p.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).getValue() * p.getAttackCooldown();
                 p.getNearbyEntities(reach,reach,reach).stream()
                     .filter(entity -> (entity.getType().equals(EntityType.ZOMBIE) || entity.getType().equals(EntityType.SKELETON)))
                     .filter(entity -> p.getLocation().distance(entity.getLocation()) < reach)
-                    .filter(entity -> p.canSee(entity))
-                    .filter(entity -> Util.getRelativeAngle(p,entity)< 2.0/180.0*Math.PI)
+                    .filter(p::hasLineOfSight)
+                    .filter(entity -> Util.getRelativeAngle(p,entity)< theta)
                     .map(entity -> (LivingEntity)entity)
-                    .forEach(entity -> entity.damage(damage));
+                    .forEach(entity -> entity.damage(damage,p));
 
         }
     }
