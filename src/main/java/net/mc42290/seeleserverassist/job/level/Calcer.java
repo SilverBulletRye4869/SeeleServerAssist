2d54d9b64f4a4d4e012e324044791f5800d0af40
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-04-22 16:38:56 +0900

    計算式変更、各値のmax値を入れられるように変更

--- src/main/java/net/mc42290/seeleserverassist/job/level/Calcer.java
+++ src/main/java/net/mc42290/seeleserverassist/job/level/Calcer.java
@@ -6,12 +6,15 @@ import org.bukkit.entity.Player;
 import java.util.UUID;
 
 public class Calcer {
-    private static final double LOGIN_TIME_COEF = 1.0;
+    private static final double LOGIN_TIME_COEF = 2.0;
     private static final double LOGIN_TIME_EXPO = 1.0;
+    private static final long LOGIN_TIME_MAX = 300;
     private static final double ATTACK_DAMAGE_COEF = 1.0;
     private static final double ATTACK_DAMAGE_EXPO = 1.0;
-    private static final double RECEIVE_DAMAGE_COEF = 1.0;
-    private static final double RECEIVE_DAMAGE_EXPO = 1.0;
+    private static final long ATTACK_DAMAGE_MAX = Long.MAX_VALUE;
+    private static final double RECEIVE_DAMAGE_COEF = 0.9;
+    private static final double RECEIVE_DAMAGE_EXPO = 0.5;
+    private static final long RECEIVE_DAMAGE_MAX = Long.MAX_VALUE;
 
     private static final double JOB_LV_EXPO = 0.8;
     private static final double PLAYER_LV_EXPO = 0.6;
@@ -22,8 +25,7 @@ public class Calcer {
         return (long)Math.pow(exp,JOB_LV_EXPO);  //近日改良
     }
 
-    public static long calcPlayerLv(Player p, long[] jobLevels){return calcPlayerLv(p.getUniqueId(),jobLevels);}
-    public static long calcPlayerLv(UUID uuid, long[] jobLevels){
+    public static long calcPlayerLv(long[] jobLevels){
         long lv = 0;
         for(long jobLv : jobLevels)lv+=jobLv;
         return (long)Math.pow(lv,PLAYER_LV_EXPO);
@@ -31,9 +33,9 @@ public class Calcer {
     public static long calcExp(long loginTime,double attackDamage, double receiveDamage, long bonusExp){
         return (long)(
                 (
-                    LOGIN_TIME_COEF * Math.pow(loginTime,LOGIN_TIME_EXPO) +
-                    ATTACK_DAMAGE_COEF * Math.pow(attackDamage,ATTACK_DAMAGE_EXPO) +
-                    RECEIVE_DAMAGE_COEF * Math.pow(receiveDamage,RECEIVE_DAMAGE_EXPO)
+                    Math.min(LOGIN_TIME_COEF * Math.pow(loginTime/60000.0,LOGIN_TIME_EXPO), LOGIN_TIME_MAX) +
+                    Math.min(ATTACK_DAMAGE_COEF * Math.pow(attackDamage,ATTACK_DAMAGE_EXPO), ATTACK_DAMAGE_MAX) +
+                    Math.min(RECEIVE_DAMAGE_COEF * Math.pow(receiveDamage,RECEIVE_DAMAGE_EXPO), RECEIVE_DAMAGE_MAX)
                 ) * bonusCoef + bonusExp
             ); //近日修正
     }
