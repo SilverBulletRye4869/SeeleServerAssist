542c38ec9cd50173b8a2c866cfb061705c762078
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-05-10 19:44:52 +0900

    JobLvを保持しておくように変更

--- src/main/java/net/mc42290/seeleserverassist/job/level/UserData.java
+++ src/main/java/net/mc42290/seeleserverassist/job/level/UserData.java
@@ -21,11 +21,21 @@ public class UserData {
     private double healAmount = 0;
     private long bonusExp = 0;
 
+    private long[] exp = new long[JobMainSystem.JOB.size()];
+    private long[] lv = new long[JobMainSystem.JOB.size()];
+    private char[] jobData;
+
 
     public UserData(Player p){
         UUID = p.getUniqueId();
         P = p;
         loginTime = System.currentTimeMillis();
+        YamlConfiguration yml = CustomConfig.getYmlByID("userdata",UUID.toString());
+        for(int i = 0;i<jobNames.length;i++){
+            exp[i] = yml.getLong("data."+jobNames[i]+".exp",0);
+            lv[i] = yml.getLong("data."+jobNames[i]+".lv",0);
+        }
+        jobData = SeeleServerAssist.getJobSystem().getJob_c(UUID);
     }
 
     public void addAD(double amount){
@@ -50,7 +60,30 @@ public class UserData {
     public long getPlayTime(){return System.currentTimeMillis()- loginTime;}
     public long getBonusExp(){return bonusExp;}
 
-    public boolean save(){
+    public void reloadJob(){
+        jobData = SeeleServerAssist.getJobSystem().getJob_c(UUID);
+    }
+    public long getJobLv(int jobNum){
+        return (0 > jobNum ||jobNum>=lv.length) ? -1 : lv[jobNum];
+    }
+
+    public boolean apply(){
+        if(jobData==null)return false;
+        Long exp = Calcer.calcExp(getPlayTime(),attackDamageAmount,receiveDamageAmount,bonusExp);
+        for(int i = 0;i<jobData.length;i++){
+            String jobName = jobNames[i];
+            if(jobName.equals("NEET")||jobData[jobData.length - i - 1] == '0')continue;
+            this.exp[i]+=exp;
+            this.lv[i] = Calcer.calcJobLv(this.exp[i]);
+        }
+        this.reset();
+
+        return true;
+    }
+
+    public boolean save(){return save(false);}
+    public boolean save(boolean toApply){
+        if(toApply)apply();
         YamlConfiguration yml = CustomConfig.getYmlByID("userdata",UUID.toString());
         JobMainSystem jobSystem =  SeeleServerAssist.getJobSystem();
         char[] jobData = jobSystem.getJob_c(UUID);
@@ -62,9 +95,8 @@ public class UserData {
             yml.set("data."+jobName+".attackDamageAmount",yml.getDouble("data."+jobName+".attackDamageAmount",0)+attackDamageAmount);
             yml.set("data."+jobName+".receiveDamageAmount",yml.getDouble("data."+jobName+".receiveDamageAmount",0)+receiveDamageAmount);
             yml.set("data."+jobName+".loginTime",yml.getLong("data."+jobName+".loginTime",0)+getPlayTime());
-            long nextExp = yml.getLong("data."+jobName+".exp",0)+exp;
-            yml.set("data."+jobName+".exp",nextExp);
-            yml.set("data."+jobName+".lv",Calcer.calcJobLv(nextExp));
+            yml.set("data."+jobName+".exp",this.exp[i]);
+            yml.set("data."+jobName+".lv",this.lv[i]);
         }
         yml.set("data.all.loginTime",yml.getLong("data.all.loginTime",0)+getPlayTime());
         reset();
