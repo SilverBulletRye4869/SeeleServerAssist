b6c51b84f56613dbcacf6257aead5d770fcf0b2b
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-05-14 11:45:28 +0900

    職業レベルによるバフスキルによるダメージ増加

--- src/main/java/net/mc42290/seeleserverassist/job/CheckMatch.java
+++ src/main/java/net/mc42290/seeleserverassist/job/CheckMatch.java
@@ -1,6 +1,7 @@
 package net.mc42290.seeleserverassist.job;
 
 import de.tr7zw.changeme.nbtapi.NBTItem;
+import net.mc42290.seeleserverassist.SeeleServerAssist;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
@@ -38,6 +39,13 @@ public class CheckMatch implements Listener {
             jobMatch = MAIN_SYSTEM.isJobMatch(p,jobNum);
             lvBonus += MAIN_SYSTEM.LEVEL_SYSTEM.getUserData(p).getJobLv(jobNum) / 100.0;
         }while (false);
+
+        if(SeeleServerAssist.getJobSystem().BUFF.BUFF_TABLE.contains(p,"attack")){
+            double attackBuffAmount = SeeleServerAssist.getJobSystem().BUFF.BUFF_TABLE.get(p,"attack");
+            e.setDamage(e.getDamage() * (1.0 + attackBuffAmount / 100.0));
+        }
+
+
         if (jobMatch)e.setDamage(e.getDamage()*lvBonus);
         else e.setDamage(Math.max(0.1*p.getAttackCooldown(),e.getDamage()*0.01));
         if(MAIN_SYSTEM.isNeet.test(p))e.setDamage(e.getDamage()/(Math.random()*8.0+2));
